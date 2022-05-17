d6a732ac24ef957c0453579260c77a391236d4ec
Bingjing-Dong <dbingjing@gmail.com>
2022-05-16 20:05:25 -0400

    add log out in User.router

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import React from 'react'; 
+import React from 'react';
 
-
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
 import MainPage from './main-page/MainPage';
 import TrainPage from './train-page/TrainPage';
 import PostPage from './post-page/PostPage';
 
-
 import DisplayInfo from './post-page/components/displayInfoGathered';
-<<<<<<< HEAD
 import Signup from './signup/signup';
 import Signin from './signin/signin';
-=======
-
-
->>>>>>> 8c97f847f6416493c35dfe0326cc21d32d96786a
 
 function App() {
   return (
     <>
-
       <Routes>
-         <Route  exact path = "/"  element = {<MainPage />} />  
-         <Route  exact path = "/station"  element = {<TrainPage />} />             
-         <Route  exact path = "/make-a-post" element = {<PostPage />} />
-         <Route  exact path = "/success" element = { <DisplayInfo />} />    
-      </Routes> 
-
+        <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/station" element={<TrainPage />} />
+        <Route exact path="/make-a-post" element={<PostPage />} />
+        <Route exact path="/success" element={<DisplayInfo />} />
+        <Route exact path="/sign-up" element={<Signup />} />
+        <Route exact path="/sign-in" element={<Signin />} />
+      </Routes>
     </>
-      
   );
 }
 
